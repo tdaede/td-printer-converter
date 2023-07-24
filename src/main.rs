121dd7f04a49da9b622c100d692c579999e2f8ab
Thomas Daede <daede003@umn.edu>
2023-07-23 23:44:43 -0700

    add cz-6pv1

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,136 @@
 use clap::Parser;
 use image::RgbImage;
+use image::imageops;
+use ipp::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::Cursor;
+use std::time::Duration;
+use std::sync::Mutex;
+use turbojpeg;
 
 mod printer;
+mod gui;
 
-use printer::Cz8pc4;
+use crate::printer::Printer;
+use printer::*;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg()]
-    input: String,
+    input: Option<String>,
 
-    #[arg()]
-    output: String,
+    #[arg(long, short)]
+    output: Option<String>,
+
+    #[arg(long)]
+    serial: Option<String>,
+
+    #[arg(long)]
+    print: Option<String>,
+
+    #[arg(long, default_value="cz-8pc4")]
+    printer: String,
+
+    #[arg(long)]
+    gui: bool,
+}
+
+fn ipp_print(print: &String, img_cropped: RgbImage) {
+    let uri: Uri = print.clone().parse().unwrap();
+    let client = IppClient::new(uri.clone());
+    let jpeg_data = turbojpeg::compress_image(&img_cropped, 100, turbojpeg::Subsamp::None).unwrap();
+    let jpeg_data_read = jpeg_data.to_vec();
+    let ipp_payload = IppPayload::new(Cursor::new(jpeg_data_read));
+    let print_operation = IppOperationBuilder::print_job(uri, ipp_payload)
+        .attribute(IppAttribute::new("document-format", IppValue::MimeMediaType("image/jpeg".to_string())))
+        .attribute(IppAttribute::new("print-scaling", IppValue::Keyword("fit".to_string())))
+        .attribute(IppAttribute::new("media-col", IppValue::Collection(vec!(
+            IppValue::MemberAttrName("media-bottom-margin".to_string()), IppValue::Integer(0),
+            IppValue::MemberAttrName("media-left-margin".to_string()), IppValue::Integer(0),
+            IppValue::MemberAttrName("media-right-margin".to_string()), IppValue::Integer(0),
+            IppValue::MemberAttrName("media-top-margin".to_string()), IppValue::Integer(0),
+        ))))
+        .build();
+    let resp = client.send(print_operation).unwrap();
+    if resp.header().status_code().is_success() {
+        eprintln!("Sent to printer!");
+    } else {
+        eprintln!("Failed to send to printer!");
+        dbg!(resp.attributes());
+    }
 }
 
 fn main() {
     let args = Args::parse();
 
-    let input_file = File::open(args.input).unwrap();
-    let mut input = BufReader::new(input_file);
+    let output = |img: &mut RgbImage, covered_x, covered_y| {
+        if covered_x == 0 || covered_y == 0 {
+            eprintln!("Page is blank, not printing!");
+            return;
+        }
+        let mut start_y: u32 = u32::MAX;
+        for (y, row) in img.rows().enumerate() {
+            for pixel in row {
+                if pixel[0] != 255 && pixel[1] != 255 && pixel[2] != 255 {
+                    start_y = y as u32;
+                    break;
+                }
+            }
+            if start_y != u32::MAX {
+                break;
+            }
+        }
+        let img_cropped = imageops::crop(img, 0, start_y, covered_x, covered_y - start_y).to_image();
+        if let Some(ref print) = args.print {
+            ipp_print(print, img_cropped);
+        } else {
+            img_cropped.save(args.output.as_ref().expect("Output filename not provided")).unwrap();
+        }
+    };
 
-    let mut img: RgbImage = Cz8pc4::create_image();
+    let mut printer: Box<dyn Printer> = match args.printer.as_str() {
+        "cz-8pc4" => Box::new(Cz8pc4::default()),
+        "cz-6pv1" => Box::new(Cz6pv1{}),
+        _ => panic!("Unknown printer"),
+    };
 
-    Cz8pc4::decode(&mut input, &mut img);
+    if args.gui {
+        gui::gui_main();
+    } else if let Some(serial_port_name) = args.serial {
+        // serial mode
+        let mut serial_port = serialport::new(&serial_port_name, 500_000).timeout(Duration::from_secs(60)).open().expect("Failed to open port");
+        eprintln!("Serial port opened on {}", serial_port_name);
+        loop {
+            let mut print_job_bytes = Vec::new();
+            let mut buf = [0; 128];
+            while let Ok(bytes_read) = serial_port.read(&mut buf) {
+                print_job_bytes.extend_from_slice(&mut buf[0..bytes_read]);
+                eprint!("Print job in progress, read {} bytes...\r", print_job_bytes.len());
+            };
+            if print_job_bytes.len() == 0 {
+                continue;
+            }
+            eprintln!("Print job of {} bytes complete            ", print_job_bytes.len());
+            let img = printer.create_image();
+            let img_mutex = Mutex::new(img);
+            let (covered_x, covered_y) = printer.decode(&mut print_job_bytes.as_slice(), &img_mutex);
+            let mut img_ = img_mutex.lock().unwrap();
+            output(&mut img_, covered_x, covered_y);
+            drop(img_);
+        }
+    } else {
+        // file mode
+        let input_file = File::open(args.input.unwrap()).unwrap();
+        let mut input = BufReader::new(input_file);
 
-    img.save(args.output).unwrap();
+        let img = printer.create_image();
+        let img_mutex = Mutex::new(img);
+
+        let (covered_x, covered_y) = printer.decode(&mut input, &img_mutex);
+
+        let mut img_ = img_mutex.lock().unwrap();
+        output(&mut img_, covered_x, covered_y);
+    }
 }
--- src/printer.rs
+++ src/printer.rs
@@ -1,22 +1,35 @@
 use std::io::Read;
+use std::sync::Mutex;
 use image::RgbImage;
 
-pub struct Cz8pc4 {
 
+pub trait Printer {
+    fn create_image(&self) -> RgbImage;
+    fn decode(&mut self, input: &mut dyn Read, img_mutex: &Mutex<RgbImage>) -> (u32, u32);
+}
+
+#[derive(Default)]
+pub struct Cz8pc4 {
+    head_y: u32,
+    covered_y: u32,
+    color: u32, // 0 = black, 1 = yellow, 2 = magenta, 3 = cyan
 }
 
 impl Cz8pc4 {
-    pub fn create_image() -> RgbImage {
-        let page_width = 2000;
-        let page_height = 2000;
-        RgbImage::new(page_width, page_height)
+    pub const PAGE_WIDTH: u32 = 2988;
+    pub const PAGE_HEIGHT: u32 = 2000;
+}
+
+impl Printer for Cz8pc4 {
+    fn create_image(&self) -> RgbImage {
+        RgbImage::from_pixel(Cz8pc4::PAGE_WIDTH, Cz8pc4::PAGE_HEIGHT, image::Rgb([255,255,255]))
     }
 
-    pub fn decode(input: &mut dyn Read, img: &mut RgbImage) {
-        let page_width = 2000;
-        let page_height = 2000;
-        let mut head_y = 0;
+    fn decode(&mut self, input: &mut dyn Read, img_mutex: &Mutex<RgbImage>) -> (u32, u32) {
+        let head_y = &mut self.head_y;
         let mut head_x = 0;
+        let mut covered_x = 0;
+        let covered_y = &mut self.covered_y;
         loop {
             let mut c: [u8; 1] = [0; 1];
             match input.read_exact(&mut c) {
@@ -30,42 +43,175 @@ impl Cz8pc4 {
                     input.read_exact(&mut b).unwrap();
                     match b[0] {
                         0x63 => {},
-                        0x25 => {},
-                        0x4d => {
-                            let mut col_count: [u8; 2] = [0; 2];
-                            input.read_exact(&mut col_count).unwrap();
-                            for x in 0..0x800 {
+                        0x23 => { // unknown 1 byte
+                            let mut b: [u8; 1] = [0; 1];
+                            input.read_exact(&mut b).unwrap();
+                        },
+                        0x25 => { // line spacing
+                            let mut asdf: [u8; 2] = [0; 2];
+                            input.read_exact(&mut asdf).unwrap();
+                            // discard line spacing for now
+                        },
+                        0x19 => {
+                            self.color = 1;
+                        },
+                        0x4c => { // unknown
+                            let mut b: [u8; 3] = [0; 3];
+                            input.read_exact(&mut b).unwrap();
+                        }
+                        0x4d => { // 48 dot
+                            let mut col_count_bytes: [u8; 2] = [0; 2];
+                            input.read_exact(&mut col_count_bytes).unwrap();
+                            let col_count = Cz8pc4::PAGE_WIDTH.min(u16::from_be_bytes(col_count_bytes) as u32);
+                            for x in 0..col_count {
                                 let mut p: [u8; 6] = [0; 6];
                                 if !input.read_exact(&mut p).is_ok() {
                                     continue;
                                 }
+                                let mut img = img_mutex.lock().unwrap();
+                                let page_width = img.width();
+                                let page_height = img.height();
                                 for (i, p_byte) in p.iter().enumerate() {
                                     for y in 0..8 {
-                                        let pixel = match p_byte >> (7-y) & 1 {
-                                            0 => image::Rgb([255,255,255]),
-                                            1 => image::Rgb([0,0,0]),
-                                            _ => unreachable!(),
-                                        };
-                                        let pixel_x = x*4 + head_x;
-                                        let pixel_y = y + head_y + i as u32 * 8;
-                                        if pixel_x < page_width && pixel_y < page_height {
-                                            img.put_pixel(pixel_x, pixel_y, pixel);
+                                        if self.color == 0 {
+                                            let pixel = match p_byte >> (7-y) & 1 {
+                                                0 => image::Rgb([255,255,255]),
+                                                1 => image::Rgb([0,0,0]),
+                                                _ => unreachable!(),
+                                            };
+                                            let pixel_x = x + head_x;
+                                            let pixel_y = y + *head_y + i as u32 * 8;
+                                            if pixel_x < page_width && pixel_y < page_height {
+                                                img.put_pixel(pixel_x, pixel_y, pixel);
+                                                *covered_y = (*covered_y).max(pixel_y);
+                                            }
+                                        } else {
+                                            if p_byte >> (7-y) & 1 != 0 {
+                                                let pixel_x = x + head_x;
+                                                let pixel_y = y + *head_y + i as u32 * 8;
+                                                if pixel_x < page_width && pixel_y < page_height {
+                                                    let mut pixel = img.get_pixel(pixel_x, pixel_y).clone();
+                                                    match self.color {
+                                                        1 => {
+                                                            pixel[2] = 0;
+                                                        },
+                                                        2 => {
+                                                            pixel[1] = 0;
+                                                        },
+                                                        3 => {
+                                                            pixel[0] = 0;
+                                                        },
+                                                        _ => {
+                                                            unreachable!();
+                                                        }
+                                                    }
+                                                    img.put_pixel(pixel_x, pixel_y, pixel);
+                                                    *covered_y = (*covered_y).max(pixel_y);
+                                                }
+                                            }
                                         }
                                     }
                                 }
+                                drop(img);
                             }
-                            head_x += 0x800;
+                            head_x += col_count;
+                            covered_x = covered_x.max(head_x);
                         },
-                        _ => {},
+                        _ => {
+                            eprintln!("Warning: unsupported escape code {:x}", b[0]);
+                        },
                     }
                 },
                 // line feed
-                0x10 => {
+                0x0a => {
                     head_x = 0;
-                    head_y += 48;
+                    *head_y += 48;
+                },
+                // carriage return / color change
+                0x0d => {
+                    head_x = 0;
+                    if self.color > 0 {
+                        self.color += 1;
+                        if self.color > 3 {
+                            self.color = 1;
+                        }
+                    }
                 }
                 _ => {}
             };
         }
+        (covered_x, *covered_y)
+    }
+}
+
+#[derive(Default)]
+pub struct Cz6pv1 {
+
+}
+
+impl Cz6pv1 {
+    const PAGE_WIDTH: u32 = 0x200;
+    const PAGE_HEIGHT: u32 = 992;
+    const Y_MIN: u16 = 0x80;
+    const Y_MAX: u16 = 0xBF;
+    const M_MIN: u16 = 0x40;
+    const M_MAX: u16 = 0x7E;
+    const C_MIN: u16 = 0x00;
+    const C_MAX: u16 = 0x3E;
+}
+
+impl Printer for Cz6pv1 {
+    fn create_image(&self) -> RgbImage {
+        RgbImage::from_pixel(Cz6pv1::PAGE_WIDTH, Cz6pv1::PAGE_HEIGHT, image::Rgb([255,255,255]))
+    }
+
+    fn decode(&mut self, input: &mut dyn Read, img_mutex: &Mutex<RgbImage>) -> (u32, u32) {
+        let mut c: [u8; 1] = [0; 1];
+        match input.read_exact(&mut c) {
+            Ok(()) => {},
+            Err(_) => return (0, 0),
+        };
+        match c[0] {
+            0xC0 => {
+                for plane in 0..3 {
+                    for y in 0..992 {
+                        let mut line = [0; 0x200];
+                        if !input.read_exact(&mut line).is_ok() {
+                            eprintln!("cz-6pv1: not enough data for image");
+                            return (0, 0)
+                        }
+                        let mut img = img_mutex.lock().unwrap();
+                        for (x, val) in line.into_iter().enumerate() {
+                            let mut pixel = img.get_pixel(x as u32, y as u32).clone();
+                            let half_offset = if y > 511 { 7 } else { 0 };
+                            let val_scaled = if y < 512 {
+                                (val & 0b00111111) * 4
+                            } else {
+                                ((((val & 0b00111111) - 7) as f32) * 4.5) as u8
+                            };
+                            match plane {
+                                0 => {
+                                    pixel[2] = val_scaled;
+                                },
+                                1 => {
+                                    pixel[1] = val_scaled;
+                                },
+                                2 => {
+                                    pixel[0] = val_scaled;
+                                }
+                                _ => unreachable!()
+                            }
+                            img.put_pixel(x as u32, y as u32, pixel);
+                        }
+                        drop(img)
+                    }
+                }
+            }
+            _ => {
+                eprintln!("cz-6pv1: unknown command");
+                return (0, 0)
+            }
+        }
+        (Cz6pv1::PAGE_WIDTH, Cz6pv1::PAGE_HEIGHT)
     }
 }
